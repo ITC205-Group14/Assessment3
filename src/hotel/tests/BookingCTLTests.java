7274263fe9cc625e6eba38a11f0788e9c94ca47d
coreyhendrey <coreyhendrey@gmail.com>
2018-09-06 06:25:55 +1000

    Started first test for BookingCTL (Testing an invalid card)
    The test runs, and I can see that it's working via the console, but I
    need to figure out how to tell JUnit the result of the test. Ask Jim.

--- src/hotel/tests/BookingCTLTests.java
+++ src/hotel/tests/BookingCTLTests.java
@@ -1,16 +1,44 @@
 package hotel.tests;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Date;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import hotel.booking.BookingCTL;
+import hotel.credit.CreditCardType;
+import hotel.entities.Hotel;
+import hotel.entities.RoomType;
 
 class BookingCTLTests
 {
+	Hotel hotel;
+	BookingCTL bookingCTL;
+
+	@BeforeEach
+	void setUp()
+	{
+		hotel = new Hotel();
+		bookingCTL = new BookingCTL(hotel);
+		hotel.addRoom(RoomType.SINGLE, 0);
+		bookingCTL.phoneNumberEntered(1);
+		bookingCTL.guestDetailsEntered("Corey", "123 Fake Street");
+		bookingCTL.roomTypeAndOccupantsEntered(RoomType.SINGLE, 1);
+		bookingCTL.bookingTimesEntered(new Date(), 3);
+	}
 
 	@Test
-	void testCreditDetailsEntered()
+	void testCreditDetailsEnteredInvalidCard()
 	{
-		fail("Not yet implemented");
+		CreditCardType cct = CreditCardType.VISA;
+		bookingCTL.creditDetailsEntered(cct, 6, 1);
 	}
+
+	@Test
+	void testCreditDetailsEnteredInvalidState()
+	{
+
+	}
+
 
 }
