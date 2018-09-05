72ae1ab10efba0efe998a2b38e022ca168ffc1b0
coreyhendrey <coreyhendrey@gmail.com>
2018-09-06 06:03:32 +1000

    Added BookingCTLTests file
    Addeded empty BookingCTLTests class

---
+++ src/hotel/tests/BookingCTLTests.java
@@ -0,0 +1,16 @@
+package hotel.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BookingCTLTests
+{
+
+	@Test
+	void testCreditDetailsEntered()
+	{
+		fail("Not yet implemented");
+	}
+
+}
