7a29790128db8a80beb9debcdcb220995e87fab4
coreyhendrey <coreyhendrey@gmail.com>
2018-09-12 06:26:06 +1000

    Added hotelCheckoutWithValidRoomId() test

--- test/hotel/entities/hotel/HotelCheckOutTests.java
+++ test/hotel/entities/hotel/HotelCheckOutTests.java
@@ -1,20 +1,28 @@
 package hotel.entities.hotel;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.verify;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
 import org.mockito.Mock;
-import org.mockito.Spy;
+import org.mockito.MockitoAnnotations;
 
 import hotel.entities.Booking;
 import hotel.entities.Hotel;
 
 class HotelCheckOutTests
 {
-	@Spy Hotel hotel = new Hotel();
+	Hotel hotel = new Hotel();
 
 	@Mock Booking booking;
+
+	@BeforeEach
+	void init()
+	{
+		MockitoAnnotations.initMocks(this);
+	}
 
 	@Test
 	void hotelCheckoutWithInvalidRoomId()
@@ -27,6 +35,20 @@ class HotelCheckOutTests
 
 		//Assert
 		assertThrows(RuntimeException.class, test);
+	}
+
+	@Test
+	void hotelCheckoutWithValidRoomId()
+	{
+		//Arrange
+		int roomId = 0;
+		hotel.activeBookingsByRoomId.put(0, booking);
+
+		//Act
+		hotel.checkout(roomId);
+
+		//Assert
+		verify(booking).checkOut();
 	}
 
 }
