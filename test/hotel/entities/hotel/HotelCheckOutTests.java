acd6400755c7405f90667290e052c30a0d555bb5
coreyhendrey <coreyhendrey@gmail.com>
2018-09-15 06:28:05 +1000

    Removed manual mock initialization and replaced with ExtendWith annotation

--- test/hotel/entities/hotel/HotelCheckOutTests.java
+++ test/hotel/entities/hotel/HotelCheckOutTests.java
@@ -3,26 +3,21 @@ package hotel.entities.hotel;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.verify;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.function.Executable;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import hotel.entities.Booking;
 import hotel.entities.Hotel;
 
+@ExtendWith(MockitoExtension.class)
 class HotelCheckOutTests
 {
 	Hotel hotel = new Hotel();
 
 	@Mock Booking booking;
-
-	@BeforeEach
-	void init()
-	{
-		MockitoAnnotations.initMocks(this);
-	}
 
 	@Test
 	void hotelCheckoutWithInvalidRoomId()
