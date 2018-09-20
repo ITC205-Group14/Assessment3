ffd3608f87b19640a5df2b1c91e29faddea53e09
coreyhendrey <coreyhendrey@gmail.com>
2018-09-21 06:45:03 +1000

    Finished Integration test with a real hotel

--- test/hotel/booking/BookingCreditDetailsEnteredIntegrationTests.java
+++ test/hotel/booking/BookingCreditDetailsEnteredIntegrationTests.java
@@ -9,6 +9,7 @@ import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -262,6 +263,76 @@ class BookingCreditDetailsEnteredIntegrationTests
 		verify(hotel).book(room, guest, arrivalDate, 0, 0, realCC);
 		verify(bookingUI).setState(BookingUI.State.COMPLETED);
 
+		when(bookingCTL.getState()).thenCallRealMethod();
+		assertEquals(BookingCTL.State.COMPLETED, bookingCTL.getState());
+	}
+
+	@Test
+	void testCreditDetailsEnteredValidCardRealCardRealRoomRealGuestRealHotel()
+	{
+		//Arrange
+		hotel = new Hotel();
+		bookingCTL.hotel = hotel;
+
+		arrivalDate = new GregorianCalendar(2018, 0, 1).getTime();
+		bookingCTL.arrivalDate = arrivalDate;
+
+		int cardNumber = 4;
+		room = new Room(0, RoomType.SINGLE);
+		bookingCTL.room = room;
+
+		guest = new Guest("Fred", "123 Fake", 555);
+		bookingCTL.guest = guest;
+
+		when(bookingCTL.getState()).thenReturn(BookingCTL.State.CREDIT);
+
+		//Make a real credit card
+		CreditCard realCC = new CreditCard(cct, cardNumber, 1);
+		//Force the helper to return this real CC
+		when(creditCardHelper.loadCreditCard(any(CreditCardType.class), anyInt(), anyInt())).thenReturn(realCC);
+
+		ArgumentCaptor<String> roomDescriptionCaptor = ArgumentCaptor.forClass(String.class);
+		ArgumentCaptor<Integer> roomNumberCaptor = ArgumentCaptor.forClass(Integer.class);
+		ArgumentCaptor<Date> arrivalDateCaptor = ArgumentCaptor.forClass(Date.class);
+		ArgumentCaptor<Integer> stayLengthCaptor = ArgumentCaptor.forClass(Integer.class);
+		ArgumentCaptor<String> guestNameCaptor = ArgumentCaptor.forClass(String.class);
+		ArgumentCaptor<String> ccVendorCaptor = ArgumentCaptor.forClass(String.class);
+		ArgumentCaptor<Integer> creditCardNumberCaptor = ArgumentCaptor.forClass(Integer.class);
+		ArgumentCaptor<Double> costNumberCaptor = ArgumentCaptor.forClass(Double.class);
+		ArgumentCaptor<Long> confirmationNumberCaptor = ArgumentCaptor.forClass(Long.class);
+
+		//Act
+		bookingCTL.creditDetailsEntered(cct, cardNumber, ccv);
+
+		//Assert
+
+		verify(bookingUI).displayConfirmedBooking(
+				roomDescriptionCaptor.capture(),
+				roomNumberCaptor.capture(),
+				arrivalDateCaptor.capture(),
+				stayLengthCaptor.capture(),
+				guestNameCaptor.capture(),
+				ccVendorCaptor.capture(),
+				creditCardNumberCaptor.capture(),
+				costNumberCaptor.capture(),
+				confirmationNumberCaptor.capture());
+
+		//We know in Java that numbers are ALWAYS initalized to 0
+		//So we can take advantage of that
+		//Verify all the captors
+		assertEquals("Single room", roomDescriptionCaptor.getValue());
+		assertTrue(0 == roomNumberCaptor.getValue());
+		assertEquals(arrivalDate, arrivalDateCaptor.getValue());
+		assertTrue(0 == stayLengthCaptor.getValue());
+		assertEquals("Fred", guestNameCaptor.getValue());
+		assertEquals(realCC.getVendor(), ccVendorCaptor.getValue());
+		assertTrue(realCC.getNumber() == creditCardNumberCaptor.getValue());
+		assertTrue(0 == costNumberCaptor.getValue());
+		assertTrue(1020180 == confirmationNumberCaptor.getValue());
+
+		verify(bookingUI).setState(BookingUI.State.COMPLETED);
+
+		//Verify we changed states
 		when(bookingCTL.getState()).thenCallRealMethod();
 		assertEquals(BookingCTL.State.COMPLETED, bookingCTL.getState());
 	}
