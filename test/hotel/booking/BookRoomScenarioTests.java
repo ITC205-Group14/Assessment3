5a0df15d2d5806a7983970ef5ec86de7a00f9c7b
coreyhendrey <coreyhendrey@gmail.com>
2018-09-22 07:35:12 +1000

    Started adding more scenario tests to BookRoom. Committing so I can merge the completed code into this branch.

--- test/hotel/booking/BookRoomScenarioTests.java
+++ test/hotel/booking/BookRoomScenarioTests.java
@@ -2,6 +2,7 @@ package hotel.booking;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyDouble;
 import static org.mockito.ArgumentMatchers.anyLong;
@@ -20,6 +21,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import hotel.credit.CreditCard;
 import hotel.credit.CreditCardType;
+import hotel.entities.Guest;
 import hotel.entities.Hotel;
 import hotel.entities.RoomType;
 
@@ -63,6 +65,74 @@ class BookRoomScenarioTests
 		//Arrange ---
 		//Check if guest really does exist
 		assertNotNull(hotel.findGuestByPhoneNumber(existingPhone));
+
+		//Act ---
+		//Check the state as we go, because why not?
+		bookingCTL.phoneNumberEntered(existingPhone);
+		assertEquals(BookingCTL.State.ROOM, bookingCTL.getState());
+		bookingCTL.roomTypeAndOccupantsEntered(rt, 1);
+		assertEquals(BookingCTL.State.TIMES, bookingCTL.getState());
+		bookingCTL.bookingTimesEntered(arrival, stayLength);
+		assertEquals(BookingCTL.State.CREDIT, bookingCTL.getState());
+		bookingCTL.creditDetailsEntered(ccType, ccNumber, ccCcv);
+		assertEquals(BookingCTL.State.COMPLETED, bookingCTL.getState());
+
+		//Assert ---
+		verify(bookingUI, times(4)).setState(any());
+		verify(hotel).isRegistered(existingPhone);
+		verify(hotel, times(2)).findGuestByPhoneNumber(existingPhone);
+		verify(bookingUI).displayGuestDetails(existingName, existingAddress, existingPhone);
+		assertEquals(rt, bookingCTL.selectedRoomType);
+		assertEquals(1, bookingCTL.occupantNumber);
+		assertEquals(arrival, bookingCTL.arrivalDate);
+		assertEquals(stayLength, bookingCTL.stayLength);
+		verify(bookingUI).displayBookingDetails(rt.getDescription(), arrival, stayLength, rt.calculateCost(arrival, stayLength));
+		verify(hotel).book(eq(bookingCTL.room), eq(bookingCTL.guest), eq(arrival), eq(stayLength), eq(1), any(CreditCard.class));
+		verify(bookingUI).displayConfirmedBooking(eq(rt.getDescription()), eq(101), eq(arrival), eq(stayLength), eq(existingName), eq(ccType.getVendor()), eq(ccNumber), anyDouble(), anyLong());
+	}
+
+	@Test
+	void NewGuestBooksIntoRoom()
+	{
+		//Arrange ---
+		//Check if guest really does NOT exist
+		assertNull(hotel.findGuestByPhoneNumber(5));
+
+		//Act ---
+		hotel.registerGuest("Newmann", "Also fake", 5);
+		//Check he registered correctly
+		assertNotNull(hotel.findGuestByPhoneNumber(5));
+		//Check the state as we go, because why not?
+		bookingCTL.phoneNumberEntered(5);
+		assertEquals(BookingCTL.State.ROOM, bookingCTL.getState());
+		bookingCTL.roomTypeAndOccupantsEntered(rt, 1);
+		assertEquals(BookingCTL.State.TIMES, bookingCTL.getState());
+		bookingCTL.bookingTimesEntered(arrival, stayLength);
+		assertEquals(BookingCTL.State.CREDIT, bookingCTL.getState());
+		bookingCTL.creditDetailsEntered(ccType, ccNumber, ccCcv);
+		assertEquals(BookingCTL.State.COMPLETED, bookingCTL.getState());
+
+		//Assert ---
+		verify(bookingUI, times(4)).setState(any());
+		verify(hotel).isRegistered(5);
+		verify(hotel, times(3)).findGuestByPhoneNumber(5);
+		verify(bookingUI).displayGuestDetails("Newmann", "Also fake", 5);
+		assertEquals(rt, bookingCTL.selectedRoomType);
+		assertEquals(1, bookingCTL.occupantNumber);
+		assertEquals(arrival, bookingCTL.arrivalDate);
+		assertEquals(stayLength, bookingCTL.stayLength);
+		verify(bookingUI).displayBookingDetails(rt.getDescription(), arrival, stayLength, rt.calculateCost(arrival, stayLength));
+		verify(hotel).book(eq(bookingCTL.room), eq(bookingCTL.guest), eq(arrival), eq(stayLength), eq(1), any(CreditCard.class));
+		verify(bookingUI).displayConfirmedBooking(eq(rt.getDescription()), eq(101), eq(arrival), eq(stayLength), eq("Newmann"), eq(ccType.getVendor()), eq(ccNumber), anyDouble(), anyLong());
+	}
+
+	@Test
+	void ExistingGuestBooksIntoUnavaliableRoom()
+	{
+		//Arrange ---
+		//Check if guest really does exist
+		assertNotNull(hotel.findGuestByPhoneNumber(existingPhone));
+		hotel.roomsByType.get(rt).get(101).book(new Guest("BookedBeforeYou", "S", 34), arrival, stayLength, 1, new CreditCard(ccType, ccNumber, ccCcv));
 
 		//Act ---
 		//Check the state as we go, because why not?
