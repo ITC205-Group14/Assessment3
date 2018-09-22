1ec28153c8370ca0a25c0c442df7553807ddf945
coreyhendrey <coreyhendrey@gmail.com>
2018-09-22 14:15:28 +1000

    Made the HotelHelper register Fred Nurke as an existing guest

--- src/hotel/HotelHelper.java
+++ src/hotel/HotelHelper.java
@@ -18,40 +18,41 @@ import hotel.utils.IOUtils;
 public class HotelHelper {
 
 	public static Hotel loadHotel() throws Exception {
-		
+
 		//SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
 		//Date date = format.parse("01-01-2001");
 		Calendar cal = Calendar.getInstance();
 		SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
 		Date date = format.parse("01-01-0001");
 		cal.setTime(date);
-		
-		
+
+
 		Hotel hotel = new Hotel();
 		hotel.addRoom(RoomType.SINGLE, 101);
 		hotel.addRoom(RoomType.DOUBLE, 201);
 		hotel.addRoom(RoomType.TWIN_SHARE, 301);
-		
+
 		Guest guest = new Guest("Fred", "Nurke", 2);
 		CreditCard card = new CreditCard(CreditCardType.VISA, 2, 2);
-		
+		hotel.registerGuest("Fed", "Nurke", 2);
+
 		Room room = hotel.findAvailableRoom(RoomType.TWIN_SHARE, date, 1);
 		long confNo = hotel.book(room, guest, date, 1, 2, card);
 		Booking booking = hotel.findBookingByConfirmationNumber(confNo);
 		hotel.checkin(confNo);
 		booking.addServiceCharge(ServiceType.ROOM_SERVICE, 7.00);
-		
+
 		IOUtils.trace("HotelHelper");
 		for (RoomType rt : RoomType.values()) {
 			Map<Integer, Room> rooms = hotel.roomsByType.get(rt);
 			for (Integer id : rooms.keySet()) {
 				IOUtils.outputln(rooms.get(id));
 			}
-		}		
+		}
 		return hotel;
 	}
 
-	
+
 	public static void saveHotel() {}
 
 }
